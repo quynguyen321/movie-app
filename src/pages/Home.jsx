bbb94b63fb754f8272497b907a8d1bdcf22ecf22
Quy Nguyen <quy.ybu@gmail.com>
2022-04-12 04:20:52 -0500

    create home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { OutlineButton } from '../components/button/Button';
 import HeroSide from '../components/hero-slide/HeroSlide';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <>
         <HeroSide/>
+        <div className='container' >
+          <div className='section mb-3'>
+            <div className='section__header mb-2'>
+              <h2>Trending Movies</h2>
+              <Link to="/movie">
+                <OutlineButton className= "small">View more
+                </OutlineButton>
+              </Link>
+            </div>
+          </div>
+        </div>
     </>
   )
 }
